7a06e351c551e63364f326f305f92f23b95d6bda
Vinicius Uliano <vgarciauliano@gmail.com>
2024-05-21 22:10:13 -0300

    listadetarefascomreact

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {useState} from "react";
 function App() {
+
+const [input, setInput] = useState('')
+const [tarefas, setTarefa] = useState([])
+function handleTask(e){
+  e.preventDefault()
+  setTarefa([...tarefas, input])
+  setInput('')
+}
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <form onSubmit={handleTask}>
+      <h1>TO-DO list</h1>
+      <label>Registrar Tarefas</label><br/>
+      <input placeholder="Insira uma tarefa"  value={input}
+      onChange={(e) => setInput(e.target.value)}></input><br/>
+      <button type="submit">Registrar</button><br/>
+    </form>
+      <div>
+        <ul>
+          {tarefas.map((tarefa)=>(
+            <li key={tarefa}>{tarefa}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
